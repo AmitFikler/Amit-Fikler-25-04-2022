e355c73f24c877a7b4bfad743bcb7d7e20408286
AmitFikler <amitfikler@gmail.com>
2022-04-26 10:34:26 +0300

    🩹 Minor

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
-
+import MainScreen from './components/MainScreen';
+import './styles/app.css';
 function App() {
   return (
     <div className='App'>
       <Header />
+      <MainScreen />
     </div>
   );
 }
