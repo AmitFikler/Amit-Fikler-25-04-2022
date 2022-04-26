6c6f445c5c2a961fbcbba481b8e5d9aea4d31a6d
AmitFikler <amitfikler@gmail.com>
2022-04-26 17:54:29 +0300

    🔗 React router

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,19 @@
 import Header from './components/Header';
 import MainScreen from './components/MainScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './styles/app.css';
+import FavoriteScreen from './components/FavoriteScreen';
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <MainScreen />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<MainScreen />} />
+          <Route path='/favorites' element={<FavoriteScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
