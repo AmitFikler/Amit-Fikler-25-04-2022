9dbc39d5a7d3da83043f590d01c5d09d4f0172e2
AmitFikler <amitfikler@gmail.com>
2022-04-27 16:20:32 +0300

    ✨ favorite screen

--- weather-app/src/components/FavoriteScreen.jsx
+++ weather-app/src/components/FavoriteScreen.jsx
@@ -1,34 +1,62 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { changeDaily, changeWeekly } from '../reducers/weatherSlice';
 import DailyWeather from './DailyWeather';
+import '../styles/favorite.css';
 
 function FavoriteScreen() {
   const weather = useSelector(({ weather }) => weather);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [favorites, setFavorites] = useState([]);
-  console.log(weather.favorites);
-  console.log(favorites);
+
   useEffect(() => {
-    setFavorites([]);
     weather.favorites.forEach((fav) => {
       axios
         .get(
           `http://dataservice.accuweather.com/currentconditions/v1/${fav.cityId}?apikey=${process.env.REACT_APP_API_KEY}`
         )
         .then((res) => {
+          console.log('in then');
           setFavorites((prev) => [
             ...prev,
-            { data: res.data[0], city: fav.city },
+            { data: res.data[0], city: fav.city, cityId: fav.cityId },
           ]);
         });
     });
   }, []);
+
+  console.log(favorites);
+
+  const handleClick = (cityId, city) => {
+    return async (dispatch) => {
+      try {
+        const daily = await axios.get(
+          `http://dataservice.accuweather.com/currentconditions/v1/${cityId}?apikey=${process.env.REACT_APP_API_KEY}`
+        );
+        const weekly = await axios.get(
+          `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityId}?apikey=${process.env.REACT_APP_API_KEY}&metric=true`
+        );
+        dispatch(changeDaily({ dailyWeather: daily.data, city }));
+        dispatch(changeWeekly({ weeklyWeather: weekly.data }));
+        navigate('/');
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+  };
   return (
     <div className='favorite-screen'>
       <div className='favorite-list'>
-        {favorites.map((fav) => {
+        {favorites.map((fav, i) => {
           return (
-            <div key={fav.city}>
+            <div
+              key={i}
+              onClick={() => dispatch(handleClick(fav.cityId, fav.city))}
+              style={{ cursor: 'pointer' }}>
               <h1>{fav.city}</h1>
               <DailyWeather daily={fav.data} />
             </div>
